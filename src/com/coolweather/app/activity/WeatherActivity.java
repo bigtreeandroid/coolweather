130732b0c753f5ad78d284308e405d8d02ea3c7c
Tony <tony@gmail.com>
2016-07-30 13:33:03 +0800

    增加后台自动更新天气功能

--- src/com/coolweather/app/activity/WeatherActivity.java
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.coolweather.app.service.AutoUpdateService;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
@@ -196,8 +197,8 @@ public  class WeatherActivity extends Activity implements OnClickListener
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
-		//Intent intent = new Intent(this, AutoUpdateService.class);
-		//startService(intent);
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 
 
